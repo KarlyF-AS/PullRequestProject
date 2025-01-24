1ed88223ff767a38287d0465c0dddcf24fd5ee35
Karly <kalbarransanchez@danielcastelao.org>
2025-01-24 09:24:26 +0100

    EXP2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hola Pull, Request");
         System.out.println("EXP1");
+        System.out.println("EXP2");
 
     }
 }
