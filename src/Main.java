de0b41c79053fd90b7ce6c282049037a92377a36
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-01-31 08:49:51 +0100

    Añadiendo formateo de numeros

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hola Pull, Request");
+        System.out.printf("Hola Pull, Request %d",23);
         System.out.println("EXP1");
         System.out.println("EXP2");
 
